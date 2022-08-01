0b6ff9187dd16ea4396e44c02924f9bf5b74b7d1
Felix Titov <titow.feliks@yandex.ru>
2022-08-01 11:34:14 +0300

    Initial Commit

---
+++ ToDoList_Realm/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ToDoList_Realm
+//
+//  Created by Felix Titov on 8/1/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
